ece40f2b04bbe163ffdcd5805e9a7d891bf68232
gggxbbb <2331490629@qq.com>
2022-05-16 10:06:24 +0800

    Up: 简化首页鉴权方式

--- routes/root.go
+++ routes/root.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"timenoteWeb/auth"
 	"timenoteWeb/web"
 )
 
 func RootRoute(r *gin.Engine) {
 
-	r.GET("/", web.HomePage)
+	r.GET("/", auth.CookieTokenAuthFunc(), web.HomePage)
 
 	r.GET("/login", web.LoginPage)
 
--- web/home.go
+++ web/home.go
@@ -2,35 +2,29 @@ package web
 
 import (
 	"github.com/gin-gonic/gin"
-	"timenoteWeb/auth"
 	. "timenoteWeb/config"
 	"timenoteWeb/loader"
 )
 
 // HomePage 主页
 func HomePage(c *gin.Context) {
-	ifLogin := auth.CookieTokenAuth(c)
-	if !ifLogin {
-		c.Redirect(302, "/login")
-	} else {
-		var data homeData
-		timenoteData, success := loader.LoadLastDataFile()
-		if !success {
-			var data errorPageData
-			data.Title = "首页"
-			data.Nickname = AppConfig.Web.Nickname
-			data.Error = errNoDataFile
-			c.HTML(errNoDataFile.Code, "error.html", data)
-			return
-		}
+	var data homeData
+	timenoteData, success := loader.LoadLastDataFile()
+	if !success {
+		var data errorPageData
 		data.Title = "首页"
-		data.Source = timenoteData.Source
 		data.Nickname = AppConfig.Web.Nickname
-		data.NoteCount = timenoteData.NoteCount()
-		data.CategoryCount = timenoteData.CategoryCount()
-		data.TodoCountTotal = timenoteData.TodoCountTotal()
-		data.TodoCountDone = timenoteData.TodoCountDone()
-		data.TodoCountUndone = timenoteData.TodoCountUndone()
-		c.HTML(200, "home.html", data)
+		data.Error = errNoDataFile
+		c.HTML(errNoDataFile.Code, "error.html", data)
+		return
 	}
+	data.Title = "首页"
+	data.Source = timenoteData.Source
+	data.Nickname = AppConfig.Web.Nickname
+	data.NoteCount = timenoteData.NoteCount()
+	data.CategoryCount = timenoteData.CategoryCount()
+	data.TodoCountTotal = timenoteData.TodoCountTotal()
+	data.TodoCountDone = timenoteData.TodoCountDone()
+	data.TodoCountUndone = timenoteData.TodoCountUndone()
+	c.HTML(200, "home.html", data)
 }
